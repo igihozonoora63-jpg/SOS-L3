7c6f78fbca6c80023c0428fd1a3e13b52d98b5f9
Noora <igihozonoora63@gmail.com>
2026-05-26 10:33:33 +0200

    my first commit on first project

---
+++ src/components/UI/footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="bg-gray-800 text-white py-4 mt-8">
+    <div class="container mx-auto text-center">
+      <p>&copy; 2024 SOS Technical High School. All rights reserved.</p>
+      <p>Contact us: <a href="mailto:tc2@sos-rwanda.org" class="text-blue-400 hover:underline">tc2@sos-rwanda.org</a></p>
+    </div>
+  </footer>
+</template>
