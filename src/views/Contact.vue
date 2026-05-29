08091f5cc5acd2b4fee335474f167ddd09d7ea87
Noora <igihozonoora63@gmail.com>
2026-05-29 08:14:32 +0200

    Updated

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,4 +10,6 @@
         <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full transition duration-300">Send Message</button>
         </form>
      </div>
+   
+      
 </template>
