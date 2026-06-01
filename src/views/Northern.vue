df38f7dfbdf382356c820865805f63f5524d49ca
Noora <igihozonoora63@gmail.com>
2026-06-01 09:04:20 +0200

    deploy solved

--- src/views/Northern.vue
+++ src/views/Northern.vue
@@ -15,7 +15,7 @@
             </div>
     
             <div class="bg-green-100 rounded-xl p-6 shadow-lg text-center">
-             <img src="/src/assets/AKagera.jpg" class="w-full h-48 object-cover rounded-lg mb-4" alt="Akagera National Park">
+             <img src="/src/assets/Akagera.jpg" class="w-full h-48 object-cover rounded-lg mb-4" alt="Akagera National Park">
              <h2 class="text-2xl font-bold mb-2">Akagera National Park</h2>
                 <p>Experience the wildlife and natural beauty of Akagera National Park in the Eastern Province.</p>
             </div>
